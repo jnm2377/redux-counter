3f7e3c8a45451c6442aa1e53808b15de7c3f9ead
Josefina Mancilla <josefinamancilla@josefinas-mbp.austin.ibm.com>
2018-07-16 15:25:55 -0500

    testing out reducer with additional increment/decrement specifications

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 function reducer(state, action) {
   if(action.type === 'INCREMENT') {
-    return state + 1;
+    return state + action.amount;
   } else if(action.type === 'DECREMENT') {
-    return state - 1;
+    return state - action.amount;
   } else {
     return state;
   }
 }
 
-const incrementAction = {type: 'INCREMENT'};
+const incrementAction = {
+  type: 'INCREMENT',
+  amount: 7
+};
 
 console.log(reducer(0, incrementAction));
 console.log(reducer(1, incrementAction));
 console.log(reducer(5, incrementAction));
 
-const unknownAction = {type: 'UNKNOWN'};
+const unknownAction = {
+  type: 'UNKNOWN'
+};
 
 console.log(reducer(5, unknownAction));
 console.log(reducer(8, unknownAction));
 
-const decrementAction = {type: 'DECREMENT'};
+const decrementAction = {
+  type: 'DECREMENT',
+  amount: 11
+};
 
 console.log(reducer(10, decrementAction));
 console.log(reducer(9, decrementAction));
